b34eea35b1015a6f769b0e22079166adcf5966c3
Flo-314 <90559933+Flo-314@users.noreply.github.com>
2021-09-28 19:54:50 -0300

    Update script.js
    principal functions are added.

--- script.js
+++ script.js
@@ -1,14 +1,89 @@
-function add(a,b){
-}
-function substract(a,b){
+let displayValue = [];
 
-}
-function multiply(a,b){
 
-}
-function divide(a,b){
+const display = document.querySelector(".display");
+const addBtn = document.querySelector(".addBtn");
+const menosBtn = document.querySelector(".menosBtn");
+const multiplyBtn = document.querySelector(".multiplyBtn");
+const divideBtn = document.querySelector(".divideBtn");
+const zeroBtn = document.querySelector(".zeroBtn");
+const oneBtn = document.querySelector(".oneBtn");
+const twoBtn = document.querySelector(".twoBtn");
+const threeBtn = document.querySelector(".threeBtn");
+const fourBtn = document.querySelector(".fourBtn");
+const fiveBtn = document.querySelector(".fiveBtn");
+const sixBtn = document.querySelector(".sixBtn");
+const sevenBtn = document.querySelector(".sevenBtn");
+const eightBtn = document.querySelector(".eightBtn");
+const nineBtn = document.querySelector(".nineBtn");
+const dotBtn = document.querySelector(".dotBtn");
+const piBtn = document.querySelector(".piBtn");
+const sinBtn = document.querySelector(".sinBtn");
+const cosBtn = document.querySelector(".cosBtn");
+const tanBtn = document.querySelector(".tanBtn");
+const eBtn = document.querySelector(".eBtn");
+const logBtn = document.querySelector(".logBtn");
+const equalBtn = document.querySelector(".equalBtn");
+const ACBtn = document.querySelector(".ACbtn");
+const deleteBtn = document.querySelector(".deleteBtn");
 
-}
-function operate (currentOperation, firstNumber, secondNumber){
+oneBtn.addEventListener("mousedown", () => (display.textContent += 1));
+twoBtn.addEventListener("mousedown", () => (display.textContent += 2));
+threeBtn.addEventListener("mousedown", () => (display.textContent += 3));
+fourBtn.addEventListener("mousedown", () => (display.textContent += 4));
+fiveBtn.addEventListener("mousedown", () => (display.textContent += 5));
+sixBtn.addEventListener("mousedown", () => (display.textContent += 6));
+sevenBtn.addEventListener("mousedown", () => (display.textContent += 7));
+eightBtn.addEventListener("mousedown", () => (display.textContent += 8));
+nineBtn.addEventListener("mousedown", () => (display.textContent += 9));
+zeroBtn.addEventListener("mousedown", () => (display.textContent += 0));
+piBtn.addEventListener("mousedown", () => (display.textContent = 3.14));
+eBtn.addEventListener("mousedown", () => (display.textContent = 2.71));
+ACBtn.addEventListener("mousedown", () => (display.textContent = undefined));
+addBtn.addEventListener("mousedown", () => addOperator("add"));
+menosBtn.addEventListener("mousedown", () => addOperator("substract"));
+multiplyBtn.addEventListener("mousedown", () => addOperator("multiply"));
+divideBtn.addEventListener("mousedown", () => addOperator("divide"));
 
+equalBtn.addEventListener("mousedown", () => operate(displayValue.currentOperator, +displayValue.secondValue, +display.textContent));
+
+
+
+
+
+
+// Basic operations
+function add(a, b) {
+  return a + b;
 }
+function substract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+// Basic operations
+
+function operate(currentOperation, firstNumber, secondNumber) {
+ let result = 0
+  if (currentOperation == "add") {
+    result = add(firstNumber, secondNumber);
+  } else if (currentOperation == "substract") {
+    result = substract(firstNumber, secondNumber);
+  } else if (currentOperation == "multiply") {
+    result = multiply(firstNumber, secondNumber);
+  } else if (currentOperation == "divide") {
+    result = divide(firstNumber, secondNumber);
+  }
+  display.textContent = result
+  return result 
+}
+function addOperator(operator){
+
+  displayValue.secondValue = display.textContent
+  displayValue.currentOperator = operator
+  display.textContent = ""
+} 
